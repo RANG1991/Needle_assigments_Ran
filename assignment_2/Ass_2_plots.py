cfc7d376c52a542979faf759dc03047076e1f5c8
Ran Galun <galun.ran@gmail.com>
2021-12-11 20:50:03 +0200

    continue EX-2

--- assignment_2/Ass_2_plots.py
+++ assignment_2/Ass_2_plots.py
@@ -1,7 +1,35 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import scipy.stats
 
-NUM_POINTS = 800
+NUM_POINTS = 500
+
+
+def draw_uniform_distribution(x1, x2, y1, y2):
+    minimums = [x1, y1]
+    maximums = [x2, y2]
+    points = np.random.uniform(low=minimums, high=maximums, size=(NUM_POINTS, 2))
+    x = points[:, 0]
+    y = points[:, 1]
+    plt.plot(x, y, 'bo')
+    plt.show()
+
+
+def draw_three_gaussians():
+    mu = 0
+    std_first = 0.5
+    std_second = 1
+    std_third = 2
+    x_first = np.linspace(mu - std_first, mu + std_first, NUM_POINTS)
+    y_first = scipy.stats.norm.pdf(x_first, mu, std_first)
+    x_second = np.linspace(mu - std_second, mu + std_second, NUM_POINTS)
+    y_second = scipy.stats.norm.pdf(x_second, mu, std_second)
+    x_third = np.linspace(mu - std_third, mu + std_third, NUM_POINTS)
+    y_third = scipy.stats.norm.pdf(x_third, mu, std_third)
+    plt.plot(x_first, y_first, "bo")
+    plt.plot(x_second, y_second, "bo")
+    plt.plot(x_third, y_third, "bo")
+    plt.show()
 
 
 def draw_two_half_circles_with_noise():
@@ -21,6 +49,8 @@ def draw_two_half_circles_with_noise():
 
 
 def main():
+    draw_uniform_distribution(-10, 2, 18, 45)
+    draw_three_gaussians()
     draw_two_half_circles_with_noise()
 
 
