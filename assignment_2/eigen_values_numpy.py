734a367712f8785fca5127653c50c82232fab528
Ran Galun <galun.ran@gmail.com>
2021-12-18 22:00:14 +0200

    more changes to the code in the plots

--- assignment_2/eigen_values_numpy.py
+++ assignment_2/eigen_values_numpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.preprocessing import normalize
 
 
 def main():
@@ -13,18 +14,36 @@ def main():
         "H": [0, -1, 0, 0, 0, 0, -1, 3, -1],
         "I": [0, 0, 0, 0, 0, 0, -1, -1, 2]
     }
+
+    bitton_vec = np.array([(5 + np.sqrt(13)) / 2, (3 + np.sqrt(13)) / 2, (1 + np.sqrt(13)) / 2,
+                           (5 + np.sqrt(13)) / -2, (5 + np.sqrt(13)) / -2, (5 + np.sqrt(13)) / -2,
+                           -1, 1, 0])
+    bitton_vec = normalize(bitton_vec[:, np.newaxis], axis=0).ravel()
+    print("bitton's vec normalized is: {}".format(bitton_vec))
+
+    bitton_vec_2 = np.array([(-3 - np.sqrt(13)) / 2, (-1 - np.sqrt(13)) / 2, -2,
+                             1, 2, (1 + np.sqrt(13)) / 2,
+                             (-1 + np.sqrt(13)) / 2, 0, 1])
+    bitton_vec_2 = normalize(bitton_vec_2[:, np.newaxis], axis=0).ravel()
+    print("bitton's vec 2 normalized is: {}".format(bitton_vec_2))
+
     l_rows = []
     keys = sorted(d.keys())
     for key in keys:
         l_rows.append(d[key])
     mat = np.array(l_rows)
     print(mat)
-    w, v = np.linalg.eig(mat)
-    w = sorted(w, reverse=True)
-    print(w)
-    print(v)
-    for i in range(len(w)):
-        print("The {} eigen value of L is: {}, with eigen vector of: {}".format(i + 1, w[i], v[i]))
+    w, v = np.linalg.eigh(mat)
+    w_list = np.real(w).tolist()
+    v_list = np.real(v).tolist()
+    list_tuples = zip(w_list, v_list)
+    list_tuples = sorted(list_tuples, key=lambda x: x[0], reverse=True)
+    for i in range(len(list_tuples)):
+        normalized_vec = normalize(np.array(list_tuples[i][1])[:, np.newaxis], axis=0).ravel()
+        print("The {} eigen value of L is: {}".format(i + 1, list_tuples[i][0]))
+        print("The normalized vector is:\n{}".format("\n".join([str(x) for x in normalized_vec.tolist()])))
+        print("The sum of the eigen vector is: {}".format(np.sum(normalized_vec)))
+        print("The norm 2 of the eigen vector is: {}".format(np.sqrt(np.sum(normalized_vec * normalized_vec))))
 
 
 if __name__ == "__main__":
