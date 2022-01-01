b7bf87120ad558962c3a146bd882c81b3c9b400c
Ran Galun <galun.ran@gmail.com>
2022-01-01 22:44:45 +0200

    starting EX3

--- assignment_3/practical_ass.py
+++ assignment_3/practical_ass.py
@@ -1,14 +1,22 @@
-from nltk.tokenize import word_tokenize
+import math
+
+from nltk import RegexpTokenizer
 from nltk.probability import FreqDist
+from matplotlib import pyplot as plt
 
 
 def main():
     words = []
+    tokenizer = RegexpTokenizer(r"\w+")
     with open("./Pride_Jane_Air_Book.txt", "r", encoding="utf8") as f:
         for row in f:
-            words += word_tokenize(row)
+            words += tokenizer.tokenize(row)
     fdist = FreqDist(words)
-    print(fdist)
+    tokens_list = fdist.most_common()
+    log_rank = [math.log(i) for i in range(1, len(tokens_list) + 1)]
+    log_freq = [math.log(x[1]) for x in tokens_list]
+    plt.plot(log_rank, log_freq)
+    plt.show()
 
 
 if __name__ == "__main__":
